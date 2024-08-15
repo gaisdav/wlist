086ed6aa557ee22c3130462e8f3f74b63b2c6c6d
gaisdav <gais.davletkildin@gmail.com>
2024-08-15 22:12:59 +0200

    deleted extra modules

--- modules/profile/entity.ts
+++
@@ -1,6 +0,0 @@
-import { type IProfileEntity } from './types';
-import { Entity } from 'typeorm';
-import { User } from '../user/entity';
-
-@Entity('wishes')
-export class Profile extends User implements IProfileEntity {}
